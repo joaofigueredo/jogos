95c7384617e885e8c327795670a0325bcdea5ad0
joaofigueredo <joaofigueredo6@gmail.com>
2026-06-06 10:28:35 -0300

    atualizacoes de perfil

--- resources/views/Login/perfil.blade.php
+++ resources/views/Login/perfil.blade.php
@@ -13,10 +13,8 @@
             <img src="{{ $jogo->url_imagem }}" class=" img-fluid mx-auto mt-3" alt="imagem de {{ $jogo->nome }}">
             <div class="card-body divBotaoFavoritos">
                 <p class="card-text text-center">{{ $jogo->nome }}</p>
-
             </div>
         </div>
-
         @endforeach
     </div>
     @endisset
@@ -27,9 +25,7 @@
                     <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                         <div class="card bg-dark text-white" style="border-radius: 1rem;">
                             <div class="card-body p-5 text-center">
-
                                 <div class="mb-md-5 mt-md-4 pb-5">
-
                                     <h2 class="fw-bold mb-2 text-uppercase">Editar perfil</h2>
                                     <form action="{{ route('login.store') }}" method="POST">
                                         @csrf
