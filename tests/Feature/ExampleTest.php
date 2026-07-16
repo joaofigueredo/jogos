f2b1ba94b3489dacf7caf05b6a458bbbf0da0018
joaofigueredo <joaofigueredo6@gmail.com>
2026-07-16 19:21:31 -0300

    Iniciar testes com Pest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,9 @@
 <?php
 
 test('the application returns a successful response', function () {
-    $response = $this->get('/');
+    $response = $this->get('/home');
 
-    $response->assertStatus(200);
+    // $response->assertStatus(200);
+
+    $response->assertSee('Jogos');
 });
